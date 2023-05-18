53c05beda8524546819734894f1b31791ef2c03e
Elijah Darkeh <elijahagbedam18@gmail.com>
2023-05-18 13:22:26 +0000

    disabled device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gdsc_challenge_app/view/onboarding_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
@@ -20,9 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      useInheritedMediaQuery: true,
-      builder: DevicePreview.appBuilder,
-      locale: DevicePreview.locale(context),
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.orange,
